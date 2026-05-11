c0f9e13db0f17f4bc0c334aa72d4a9b528b33a7c
wnm0601 <wnm0601@gmail.com>
2026-05-11 12:42:55 +0900

    Add files via upload

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,3 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+export function cn(...inputs: (string | undefined | null | false)[]) {
+  return inputs.filter(Boolean).join(" ");
 }
