52b8b30425ffa304bc1a5588b96277613759de61
wnm0601 <wnm0601@gmail.com>
2026-05-11 12:30:01 +0900

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import Hero from "@/components/sections/Hero";
-import About from "@/components/sections/About";
-import Problem from "@/components/sections/Problem";
-import Solutions from "@/components/sections/Solutions";
-import Technology from "@/components/sections/Technology";
-import Robotics from "@/components/sections/Robotics";
-import Projects from "@/components/sections/Projects";
-import CollaborationHub from "@/components/sections/CollaborationHub";
-import Process from "@/components/sections/Process";
-import Contact from "@/components/sections/Contact";
+import Hero from "../components/sections/Hero";
+import About from "../components/sections/About";
+import Problem from "../components/sections/Problem";
+import Solutions from "../components/sections/Solutions";
+import Technology from "../components/sections/Technology";
+import Robotics from "../components/sections/Robotics";
+import Projects from "../components/sections/Projects";
+import CollaborationHub from "../components/sections/CollaborationHub";
+import Process from "../components/sections/Process";
+import Contact from "../components/sections/Contact";
 
 export default function Home() {
   return (
